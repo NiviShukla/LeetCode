d52d3cfc5ffcd3a9c2f6444083574e11ae73d194
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2022-12-16 23:03:19 +0530

    Add files via upload

---
+++ 75. Sort Colors/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        //using Dutch National Flag Algorithm
+        int low = 0,mid=0;
+        int high = nums.size()-1;
+
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+            else
+            mid++;
+        }
+
+    }
+};
