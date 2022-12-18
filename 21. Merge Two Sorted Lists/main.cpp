6c0efaa0e9413e98ba45018b62f712c2c098f910
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2022-12-18 21:21:31 +0530

    Update main.cpp

--- 21. Merge Two Sorted Lists/main.cpp
+++ 21. Merge Two Sorted Lists/main.cpp
@@ -13,7 +13,9 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode* p1 = list1;
         ListNode* p2 = list2;
-
+        /*Taking this dummy node to return the answer
+        3rd pointer at dummy node that will get ahead
+        */
         ListNode* dummy = new ListNode(-1);
         ListNode* p3 = dummy;
 
@@ -31,13 +33,14 @@ public:
             }
             p3= p3->next;
         }
-
+        //if list2 gets to NULL or is empty
         while(p1 != NULL)
         {
             p3->next = p1;
             p1 = p1->next;
             p3 = p3->next;
         }
+        //if list1 gets to NULL or is empty
         while(p2 != NULL)
         {
             p3->next = p2;
