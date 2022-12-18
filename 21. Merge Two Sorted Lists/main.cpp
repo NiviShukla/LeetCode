a7a0524887c2702d96f39971b83b550c8fbaa660
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2022-12-18 21:19:29 +0530

    Add files via upload

---
+++ 21. Merge Two Sorted Lists/main.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* p1 = list1;
+        ListNode* p2 = list2;
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* p3 = dummy;
+
+        while(p1 != NULL && p2 != NULL)
+        {
+            if(p1->val <p2->val)
+            {
+                p3->next = p1;
+                p1=p1->next;
+            }
+            else
+            {
+                p3->next = p2;
+                p2 =p2->next;
+            }
+            p3= p3->next;
+        }
+
+        while(p1 != NULL)
+        {
+            p3->next = p1;
+            p1 = p1->next;
+            p3 = p3->next;
+        }
+        while(p2 != NULL)
+        {
+            p3->next = p2;
+            p2 = p2->next;
+            p3 = p3->next;
+        }
+
+        return dummy->next;
+    }
+};
