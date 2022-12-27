df35526390f5d4f2e8d4ae2249dcf7ecda5cbc96
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2022-12-28 00:00:20 +0530

    Update main.cpp

--- 179. Largest Number/main.cpp
+++ 179. Largest Number/main.cpp
@@ -18,16 +18,26 @@ public:
         }
         if( x == nums.size())
         return "0";
-
+        
+        //converting the vector into string for concatination
         vector<string> s(nums.size()," ");
         for(int i =0;i<nums.size();i++)
         {
             s[i]=to_string(nums[i]);
         }
-
+        /*
+        soritng the vector with 2 strings being compared 
+        each time and then swapping
+        Ex: [9,30,33]
+        9 and 30 will give 930 as 309 is small
+        then for 30 and 33
+        3330 will be returned from thr check function
+        as 3033 is small
+        */
         sort(s.begin(),s.end(),check);
 
         string ans = "";
+        //adding thr new string vector into answer string
         for(int i = 0;i<s.size();i++)
         {
             ans += (s[i]);
