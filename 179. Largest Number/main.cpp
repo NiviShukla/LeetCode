1797d160e308b8aa6bc4cb280eea26e7ea90f275
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2022-12-27 23:56:06 +0530

    Add files via upload

---
+++ 179. Largest Number/main.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    static bool check(string& s1, string& s2)
+    {
+        return (s1+s2)>=(s2+s1);
+    }
+
+    string largestNumber(vector<int>& nums) {
+
+        //if the array is 0 then printing 0
+        int x =0;
+        for(int i =0;i<nums.size();i++)
+        {
+            if(nums[i] == 0)
+            {
+                x++;
+            }
+        }
+        if( x == nums.size())
+        return "0";
+
+        vector<string> s(nums.size()," ");
+        for(int i =0;i<nums.size();i++)
+        {
+            s[i]=to_string(nums[i]);
+        }
+
+        sort(s.begin(),s.end(),check);
+
+        string ans = "";
+        for(int i = 0;i<s.size();i++)
+        {
+            ans += (s[i]);
+        }
+        return ans;
+    }
+};
